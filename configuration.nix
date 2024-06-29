13e2c4e8556597dc38408482434706b5565c724d
berker-z <berker.zor@gmail.com>
2024-06-29 15:31:46 +0300

    regular update

--- configuration.nix
+++ configuration.nix
@@ -80,13 +80,15 @@ XDG_SESSION_TYPE = "wayland";
 #keyring stuff?
 services.gnome.gnome-keyring.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+
   users.users.berkerz = {
     isNormalUser = true;
     shell = pkgs.fish;
     description = "berkerz";
     extraGroups = [ "networkmanager" "sound"  "wheel" ];
+    initialPassword = "1234";
     packages = with pkgs; [];
+    
   };
 
   # Allow unfree packages
@@ -154,7 +156,7 @@ services.gnome.gnome-keyring.enable = true;
   networkmanagerapplet
   nix-prefetch
   nix-prefetch-git
-  libsForQt5.qt5.qtgraphicaleffects
+  libsForQt5.qt5.qtgraphicaleffects # sddm doesn't work without this
   ];
   
   ##############################################################
@@ -227,11 +229,6 @@ programs.git = {
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
