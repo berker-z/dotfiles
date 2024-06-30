6c9842a8fcff6101b5990d6c9f61d0940f167ff1
berker-z <berker.zor@gmail.com>
2024-06-30 09:01:36 +0300

    regular update

--- flake.nix
+++ flake.nix
@@ -1,18 +1,20 @@
 {
   description = "A very basic flake";
 
-  inputs = {
+  inputs = 
+  {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-      sddm-sugar-candy-nix = {
-    url = "gitlab:Zhaith-Izaliel/sddm-sugar-candy-nix";
+    sddm-sugar-candy-nix = 
+       {
+       url = "gitlab:Zhaith-Izaliel/sddm-sugar-candy-nix";
 
-  };
+       };
 
-	home-manager = {
-url = "github:nix-community/home-manager/master";
-inputs.nixpkgs.follows = "nixpkgs";
-
-};
+	home-manager = 
+       {
+       url = "github:nix-community/home-manager/master";
+       inputs.nixpkgs.follows = "nixpkgs";
+      };
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, sddm-sugar-candy-nix, ... }: {
@@ -40,6 +42,31 @@ home-manager.users.berkerz = import ./home.nix;
 }
 ];
 };
+
+#I don't know how to make two of these without copying everything, there is probably a much more elegant way of doing it
+	laptop = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+    sddm-sugar-candy-nix.nixosModules.default
+    ./configuration.nix
+    ./hosts/laptop/default.nix
+      {
+        nixpkgs = {
+          overlays = [
+            sddm-sugar-candy-nix.overlays.default
+          ];
+        };
+      }
+home-manager.nixosModules.home-manager {
+home-manager.useGlobalPkgs = true;
+home-manager.useUserPackages = true;
+
+home-manager.users.berkerz = import ./home.nix;
+}
+];
+};
+
+
 };
 };
 }
