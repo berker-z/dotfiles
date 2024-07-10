35d1a5acd7df77463487d274ef30a80acc0bf8ac
berker-z <berker.zor@gmail.com>
2024-07-10 12:27:47 +0300

    regular update

--- flake.nix
+++ flake.nix
@@ -4,12 +4,11 @@
   inputs = 
   {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     sddm-sugar-candy-nix = 
        {
        url = "gitlab:Zhaith-Izaliel/sddm-sugar-candy-nix";
-
        };
-
 	home-manager = 
        {
        url = "github:nix-community/home-manager/master";
@@ -17,7 +16,7 @@
       };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, sddm-sugar-candy-nix, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, nixos-hardware, sddm-sugar-candy-nix, ... }: {
 	nixosConfigurations = {
 	nixos = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
@@ -48,6 +47,7 @@ home-manager.users.berkerz = import ./home.nix;
     system = "x86_64-linux";
     modules = [
     sddm-sugar-candy-nix.nixosModules.default
+    nixos-hardware.nixosModules.asus-zephyrus-ga401
     ./configuration.nix
     ./hosts/laptop/default.nix
       {
