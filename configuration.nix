ac9c782956e0224058e1b8a8eeed4d06a1fdd891
berker-z <berker.zor@gmail.com>
2024-09-24 21:07:45 +0300

    regular update

--- configuration.nix
+++ configuration.nix
@@ -257,27 +257,6 @@ programs.git = {
 };
 
 
-# RClone Google Drive service
-#systemd.services.rclone-gdrive-mount = {
-#  # Ensure the service starts after the network is up
-#  wantedBy = [ "multi-user.target" ];
-#  after = [ "network-online.target" ];
-#  requires = [ "network-online.target" ];
-#
-#  # Service configuration
-#  serviceConfig = {
-#    Type = "simple";
-#    ExecStartPre = "/run/current-system/sw/bin/mkdir -p /home/berkerz/gDrive"; # Creates folder if didn't exist
-#    ExecStart = "${pkgs.rclone}/bin/rclone mount drive: /home/berkerz/gDrive"; # Mounts
-#    ExecStop = "/run/current-system/sw/bin/fusermount -u /home/berkerz/gDrive"; # Dismounts
-#    Restart = "on-failure";
-#    RestartSec = "10s";
-#    User = "berkerz";
-#    Group = "users";
-#    Environment = [ "PATH=/run/wrappers/bin/:$PATH" ]; # Required environments
-#  };
-#};
-
 systemd.services.rclone-gdrive-sync = {
   # Ensure the service starts after the network is up
   wantedBy = [ "multi-user.target" ];
@@ -305,6 +284,8 @@ systemd.timers.rclone-gdrive-sync = {
     Unit = "rclone-gdrive-sync.service";
   };
 };
+
+
 
 
   # Some programs need SUID wrappers, can be configured further or are
