ae8fd5d4d8407f23be80aeeec68e91eb8a4f5e4e
berker-z <berker.zor@gmail.com>
2024-09-24 18:52:22 +0300

    regular update

--- configuration.nix
+++ configuration.nix
@@ -257,6 +257,26 @@ programs.git = {
 };
 
 
+# RClone Google Drive service
+systemd.services.rclone-gdrive-mount = {
+  # Ensure the service starts after the network is up
+  wantedBy = [ "multi-user.target" ];
+  after = [ "network-online.target" ];
+  requires = [ "network-online.target" ];
+
+  # Service configuration
+  serviceConfig = {
+    Type = "simple";
+    ExecStartPre = "/run/current-system/sw/bin/mkdir -p /home/berkerz/gDrive"; # Creates folder if didn't exist
+    ExecStart = "${pkgs.rclone}/bin/rclone mount drive: /home/berkerz/gDrive"; # Mounts
+    ExecStop = "/run/current-system/sw/bin/fusermount -u /home/berkerz/gDrive"; # Dismounts
+    Restart = "on-failure";
+    RestartSec = "10s";
+    User = "berkerz";
+    Group = "users";
+    Environment = [ "PATH=/run/wrappers/bin/:$PATH" ]; # Required environments
+  };
+};
 
 
 
