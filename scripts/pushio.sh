55fbe52668d11751eea1b922f082fe8220c9da8e
berker-z <berker.zor@gmail.com>
2024-09-30 17:13:43 +0300

    regular update

--- scripts/pushio.sh
+++ scripts/pushio.sh
@@ -8,4 +8,4 @@ git commit -m "regular update" || exit 1
 git push -u origin main || exit 1
 
 echo "Pushed to git successfully."
-notify-send "NixOS Configuration" "Updated from GitHub."
+notify-send "NixOS Configuration" "Pushed changes to GitHub."
