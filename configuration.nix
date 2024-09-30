e5a2168a61c454c26704cf20479ea2144f442ba5
berker-z <berker.zor@gmail.com>
2024-09-30 19:43:08 +0300

    regular update

--- configuration.nix
+++ configuration.nix
@@ -275,9 +275,29 @@ systemd.services.rclone-gdrive-sync = {
   serviceConfig = {
     Type = "oneshot";
     ExecStartPre = "/run/current-system/sw/bin/mkdir -p /home/berkerz/gDrive"; # Creates folder if it doesn't exist
-    ExecStart = "${pkgs.rclone}/bin/rclone bisync --resync drive: /home/berkerz/gDrive"; # Syncs
+    ExecStart = "${pkgs.rclone}/bin/rclone bisync --resync --resilient drive: /home/berkerz/gDrive"; # Syncs
     User = "berkerz";
     Group = "users";
+    Environment = [ "PATH=/run/wrappers/bin/:$PATH" ]; # Required environments
+  };
+};
+
+systemd.paths."rclone-gdrive-sync.path" = {
+  wantedBy = [ "multi-user.target" ];
+  pathConfig = {
+    PathModified = "/home/berkerz/gDrive";  # Trigger sync when files change
+    Unit = "rclone-notif.service";   # This points to the sync service
+  };
+
+};
+
+systemd.services.rclone-notif = {
+
+  serviceConfig = {
+    Type = "oneshot";  # Run once when triggered
+    #ExecStart = "/run/current-system/sw/bin/echo 'Change detected in gDrive: You made a change in /home/berkerz/gDrive!' | systemd-cat";  # Log message to systemd journal
+    ExecStart = "/run/current-system/sw/bin/rclone bisync --recover drive: /home/berkerz/gDrive";
+    User = "berkerz";  # Run as your user
     Environment = [ "PATH=/run/wrappers/bin/:$PATH" ]; # Required environments
   };
 };
@@ -285,11 +305,10 @@ systemd.services.rclone-gdrive-sync = {
 # Add a timer to run the sync service periodically
 systemd.timers.rclone-gdrive-sync = {
   wantedBy = [ "timers.target" ];
-  partOf = [ "rclone-gdrive-sync.service" ];
   timerConfig = {
     OnBootSec = "1m";
     OnUnitActiveSec = "1m";
-    Unit = "rclone-gdrive-sync.service";
+    Unit = "rclone-notif.service";
   };
 };
 
