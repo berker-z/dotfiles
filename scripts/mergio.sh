c9cde55d8f7a2cab18f31728cd631703316a37ba
berker-z <berker.zor@gmail.com>
2024-09-30 18:48:28 +0300

    regular update

--- scripts/mergio.sh
+++ scripts/mergio.sh
@@ -9,7 +9,6 @@ git merge origin/main || exit 1
 echo "Git fetch and merge from origin/main completed successfully."
 
 
-
 #!/usr/bin/env bash
 
 set -euo pipefail
@@ -21,20 +20,10 @@ cd "$target_directory" || exit 1
 # Fetch the latest changes
 git fetch origin main || exit 1
 
-# Check if there are any changes
-if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/main)" ]; then
-    # Stash any local changes
-    git stash
+git stash || exit 1
 
-    # Reset to the latest commit on origin/main
-    git reset --hard origin/main
+# Reset to the latest commit on origin/main
+git reset --hard origin/main || exit 1
 
-    echo "NixOS configuration synced from Github."
-    notify-send "NixOS Configuration" "Updated from GitHub."
-    # Optionally, you can add a notification here
-    # notify-send "NixOS Configuration" "Updated and rebuilt from GitHub"
-else
-    echo "Already up to date. No changes to apply."
-    notify-send "NixOS Configuration" "No changes on GitHub."
-
-fi
+echo "NixOS configuration synced from Github."
+notify-send "NixOS Configuration" "Updated from GitHub."
