7dd84ad6997eaa8b3fe03d8c572a3f2ae2eb0568
berker-z <berker.zor@gmail.com>
2024-11-11 10:01:01 +0300

    regular update

--- scripts/mergio.sh
+++ scripts/mergio.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-target_directory=~/.dotfiles
-
-cd "$target_directory" || exit 1
-git fetch origin main || exit 1
-git merge origin/main || exit 1
-
-echo "Git fetch and merge from origin/main completed successfully."
-
-
-#!/usr/bin/env bash
-
 set -euo pipefail
 
 target_directory=~/.dotfiles
