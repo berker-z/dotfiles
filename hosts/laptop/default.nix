c8b6ee3bce8e9625f642728dd13acb457691061b
berker-z <berker.zor@gmail.com>
2025-01-12 23:13:28 +0300

    regular update

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,45 +1,44 @@
-
-
-{ lib, config, pkgs, ... }:
-
 {
-    imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
-# Allow unfree packages
+  # Allow unfree packages
 
-# NVIDIA drivers are unfree.  
-services.xserver.videoDrivers = [ "nvidia" ]; # If you are using a hybrid laptop add its iGPU manufacturer
-hardware.graphics = {  
-  enable = true;  
-#  driSupport = true;  
-#  driSupport32Bit = true;  
-};
+  # NVIDIA drivers are unfree.
+  services.xserver.videoDrivers = ["nvidia"]; # If you are using a hybrid laptop add its iGPU manufacturer
+  hardware.graphics = {
+    enable = true;
+    #  driSupport = true;
+    #  driSupport32Bit = true;
+  };
 
+  environment.sessionVariables = {
+    GSK_RENDERER = "ngl";
+  };
 
-environment.sessionVariables = {
-  
-  GSK_RENDERER = "ngl";
-};
+  #20bdb3cd-f7e8-4811-8200-ca2d7c232ad1
 
-
-#20bdb3cd-f7e8-4811-8200-ca2d7c232ad1
-
-networking.hostName = "laptop";
-hardware.nvidia = {
-  # Enable modesetting for Wayland compositors (hyprland)
-  modesetting.enable = true;
-  # Use the open source version of the kernel module (for driver 515.43.04+)
-  open = false;
-  # Enable the Nvidia settings menu
-  nvidiaSettings = true;
-  # Select the appropriate driver version for your specific GPU
-  package = config.boot.kernelPackages.nvidiaPackages.stable;
-};
+  networking.hostName = "laptop";
+  hardware.nvidia = {
+    # Enable modesetting for Wayland compositors (hyprland)
+    modesetting.enable = true;
+    # Use the open source version of the kernel module (for driver 515.43.04+)
+    open = false;
+    powerManagement.enable = false;
+    #powerManagement.finegrained = true;
+    # Enable the Nvidia settings menu
+    nvidiaSettings = true;
+    # Select the appropriate driver version for your specific GPU
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
+  };
 
   # Enable Hyprland
   programs.hyprland.enable = true;
-  
 }
